97dfa5724675dc6dedf9709b7d75d33bc3372762
Esmaeil <esmuilsa@yahoo.com>
2025-09-16 22:33:27 +0330

    test(ui): refactor snapshot tests for TextView across light/dark and portrait/landscape

--- TDDinUIDemoTests/TDDinUIDemoTests.swift
+++ TDDinUIDemoTests/TDDinUIDemoTests.swift
@@ -13,29 +13,49 @@ import SwiftUI
 @MainActor
 struct TDDinUIDemoTests {
 
-    @Test("a TextView on iPhone - light Mode - Portrait")
+    @Test("a TextView on iPhone - light/dark Mode - Portrait/Landscape")
     func textView_iPhone() async throws {
         
-        let text: AttributedString = {
-            var attr = AttributedString("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.")
-            if let range = attr.range(of: "ipsum") {
-                attr[range].backgroundColor = .yellow
-            }
+            let controller = UIHostingController(rootView: TextView(text: .constant(AttributedString(""))))
+            // Light Mode
+            controller.overrideUserInterfaceStyle = .light
+            
+            assertSnapshot(of: controller,
+                           as: .image(on: .iPhone13Pro(.portrait)),
+                           named: "portrait_lightMode",
+                           record: true
+            
+            )
+        
+            // Portrait - Dark Mode
+            controller.overrideUserInterfaceStyle = .dark
+            assertSnapshot(of: controller,
+                           as: .image(on: .iPhone13Pro(.portrait)),
+                           named: "portrait_darkMode",
+                           record: true
+            
+            )
+            
+            // Landscape - LightMode
+            controller.overrideUserInterfaceStyle = .light
 
-            return attr
-        }()
-        
-        let controller = UIHostingController(rootView: TextView(text: .constant(text)))
-        // Light Mode
-        controller.overrideUserInterfaceStyle = .light
-        
-        assertSnapshot(of: controller,
-                       as: .image(on: .iPhone13Pro(.portrait)),
-                       named: "portrait-lightMode",
-                       record: true
-        
-        )
-    }
+            assertSnapshot(of: controller,
+                           as: .image(on: .iPhone13Pro(.landscape)),
+                           named: "landscape_lightMode",
+                           record: true
+            
+            )
+            
+            // Landscape - DarkMode
+            controller.overrideUserInterfaceStyle = .dark
+
+            assertSnapshot(of: controller,
+                           as: .image(on: .iPhone13Pro(.landscape)),
+                           named: "landscape_darkMode",
+                           record: true
+            
+            )
+        }
 
 }
 
